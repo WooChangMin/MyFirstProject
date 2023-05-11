e5063139244568441d6def516ae6bb984669d362
우창민 <rebuilding33@gmail.com>
2023-05-11 21:49:33 +0900

    feat : 과제

---
+++ Assets/Homework/2023_05_11/StartJump.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartJump : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.AddForce(Vector3.up * 5, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
